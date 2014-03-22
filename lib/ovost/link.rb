1529e5077fefd43c0ff8e21b76dfbd2d79fcd3c4
vagrant <vagrant@student-dev-box>
2014-03-22 21:59:08 +0000

    Minor refactor

--- lib/ovost/link.rb
+++ lib/ovost/link.rb
@@ -8,9 +8,7 @@ class OVOST::Link
 
     if (destination_url.length < 5)
       @destination_url = "http://ovo.st"
-    elsif destination_url[0..6].downcase == "http://"
-      @destination_url = destination_url
-    elsif destination_url[0..7].downcase == "https://"
+    elsif destination_url[0..6].downcase == "http://" || destination_url[0..7].downcase == "https://"
       @destination_url = destination_url
     else @destination_url = "http://#{destination_url}"
     end
