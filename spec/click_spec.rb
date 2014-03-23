b20c54ba598c68ffcd7f94060de3df26b2c3b5a6
vagrant <vagrant@student-dev-box>
2014-03-23 18:17:02 +0000

    Simple failing test for Click class initialize method

--- spec/click_spec.rb
+++ spec/click_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe "Click" do
-
+  it "Takes in a link_id and stores it along with the time of click" do
+    click = OVOST::Click.new('1234')
+    expect(click.link_id).to eq('1234')
+    expect(click.time).to be_a(Time)
+  end
 end
