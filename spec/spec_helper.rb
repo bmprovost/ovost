2bb5dc509a6f756be59a2471bc949fe637de5f36
vagrant <vagrant@student-dev-box>
2014-03-26 13:16:57 +0000

    Pass tests using singleton class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require './lib/ovost.rb'
+
+RSpec.configure do |config|
+  # Reset singleton instance before every test
+  config.before(:each) do
+    Singleton.__init__(OVOST::Database)
+  end
+end
